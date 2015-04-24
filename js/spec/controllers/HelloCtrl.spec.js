648ef5b1c34ef6f4ac02f1f1525bb99744b949ae
jbnizet <jb@ninja-squad.com>
2015-04-24 12:12:04 +0200

    Test unitaire avec

--- js/spec/controllers/HelloCtrl.spec.js
+++ js/spec/controllers/HelloCtrl.spec.js
@@ -1,23 +1,46 @@
 describe('HelloCtrl', function() {
 
-    var $scope;
+    var $scope, $httpBackend;
 
     beforeEach(module('controllers'));
 
-    beforeEach(inject(function($controller, $rootScope) {
+    beforeEach(inject(function($controller, $rootScope, _$httpBackend_) {
         $scope = $rootScope.$new();
+        $httpBackend = _$httpBackend_;
         $controller('HelloCtrl', {
             $scope: $scope
         });
     }));
 
-    it('should say Hello World when no argument', function() {
-        $scope.hello();
-        expect($scope.message).toBe('Hello World');
+    it('should set orgError to false and get org if valid login', function() {
+        $scope.orgError = true;
+
+        var org = {
+            name: 'Ninja Squad'
+        };
+        $httpBackend.expectGET('https://api.github.com/orgs/Ninja-Squad').respond(org);
+
+        $scope.showOrg('Ninja-Squad');
+        expect($scope.orgError).toBeFalsy();
+
+        $httpBackend.flush();
+
+        expect($scope.org).toEqual(org);
     });
 
-    it('should say Hello JB when JB as argument', function() {
-        $scope.hello('JB');
-        expect($scope.message).toBe('Hello JB');
+    it('should set orgError to false and get org if invalid login', function() {
+        $scope.orgError = false;
+        $scope.org = {
+            name: 'Ninja Squad'
+        };
+
+        $httpBackend.expectGET('https://api.github.com/orgs/Not-Existing').respond(404);
+
+        $scope.showOrg('Not-Existing');
+
+        $httpBackend.flush();
+
+        expect($scope.orgError).toBeTruthy();
+        expect($scope.org).toBeFalsy();
     });
 });
--- js/src/controllers/HelloCtrl.js
+++ js/src/controllers/HelloCtrl.js
@@ -1,6 +1,12 @@
-angular.module('controllers').controller('HelloCtrl', function($scope) {
-    $scope.hello = function(name) {
-        var dest = name || 'World';
-        $scope.message = 'Hello ' + dest;
+angular.module('controllers').controller('HelloCtrl', function($scope, $http) {
+    $scope.showOrg = function(login) {
+        $scope.orgError = false;
+        $scope.org = null;
+        $http.get('https://api.github.com/orgs/' + login).success(function(data) {
+            $scope.org = data;
+        }).error(function() {
+            $scope.org = null;
+            $scope.orgError = true;
+        })
     };
 });
